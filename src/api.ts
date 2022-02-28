879a87febf3013cd262f37cc717c439df2ea2e78
Ali Sawari <ali.dragon.swr@gmail.com>
2022-03-01 01:31:20 +0300

    new function to extract the data

--- src/api.ts
+++ src/api.ts
@@ -1,40 +1,56 @@
 import axios from "axios";
 
-
 const URLS = {
   yomama: 'https://yomomma-api.herokuapp.com/jokes',
   excuses: 'https://excuser.herokuapp.com/v1/excuse',
-  facts: 'https://api.aakhilv.me/facts',
+  facts: 'https://api.aakhilv.me/fun/facts',
   norris: 'https://api.chucknorris.io/jokes/random',
   nerd: 'https://corporatebs-generator.sameerkumar.website/'
 }
 
 
-export async function getJoke () {
+function parseResult(result: any, keyword?: string) {
+  if (result.length) {
+    result = result[0];
+  }
+  if (result && Object.keys(result).length) {
+    if (keyword) return result[keyword];
+    const [mine] = Object.keys(result);
+    return result[mine];
+  } else return result;
+}
+
+
+export async function getJoke() {
   const response = await axios.get(URLS.yomama);
-  return response.data.joke;
+  // return response.data.joke;
+  return parseResult(response.data, "joke");
 }
 
 
-export async function getExcuse(){
+export async function getExcuse() {
   const response = await axios.get(URLS.excuses);
-  return response.data[0].excuse;
+  // return response.data[0].excuse;
+  return parseResult(response.data, "excuse");
 }
 
 
-export async function getFact(){
+export async function getFact() {
   const response = await axios.get(URLS.facts);
-  return response.data[0];
+  // return response.data.data[0];
+  return parseResult(response.data, "data")[0];
 }
 
 
-export async function getNorris(){
+export async function getNorris() {
   const response = await axios.get(URLS.norris);
-  return response.data.value;
+  // return response.data.value;
+  return parseResult(response.data, "value");
 }
 
 
-export async function getNerd(){
+export async function getNerd() {
   const response = await axios.get(URLS.nerd);
-  return response.data.phrase;
+  // return response.data.phrase;
+  return parseResult(response.data, "phrase");
 }
